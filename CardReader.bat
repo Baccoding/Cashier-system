aaf78f8e4963e58457a5cb98aa1208b086b807dd
Baccoding <111810427+Baccoding@users.noreply.github.com>
2024-02-27 15:32:24 +0200

    Add files via upload

---
+++ CardReader.bat
@@ -0,0 +1,1 @@
+"D:\Java\jdk1.8.0_202\bin\java.exe" -jar "D:\IntelliJ IDEA Community Edition 2022.2.1\Projects\project-pvp22-group-fyra-1\src\main\resources\api\CardReader.jar"
---
+++ CashBox.bat
@@ -0,0 +1,2 @@
+cd "D:\IntelliJ IDEA Community Edition 2022.2.1\Projects\project-pvp22-group-fyra-1\src\main\resources\api"
+"D:\Java\jdk1.8.0_202\bin\java.exe" -jar CashBox.jar
